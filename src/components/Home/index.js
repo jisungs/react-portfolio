183d42b84a9ea737e902bc761dcb3efd297a4ef7
Jisung Smart <jisung.smart@gmail.com>
2022-11-11 14:49:54 +0900

    add useeffect but screen is disapper can not fix the problem

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,50 @@
 import LogoTitle from '../../assets/images/logo-s.png';
-import { Link } from 'react-router-dom'
-import './index.scss'
+import { Link } from 'react-router-dom';
+import './index.scss';
+import { useState, useEffect } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [ letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['l','o','b','o','d','a','n'];
+    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.'];
+    
+    useEffect(() => {
+        return setTimeout(() => {
+          setLetterClass('text-animate-hover')
+        }, 4000)
+      }, []);
+   
+
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/>I'm
-                <img src={LogoTitle} alt="" />
-                Jisung
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
                 <br/>
-                Web Developer
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15}
+                 />
+                <br/>
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                 idx={22}   
+                />
                 </h1>
                 <h2> Fullstack Developer / Online business / Blogger </h2>
                 <Link to="/contack" className="flat-button">CONTACT ME</Link>
 
             </div>
         </div>
+        </>
 
-    );
+    )
 }
 
 export default Home;
