0327af55ca44aae7ac11d738b395b9aff35e8344
Jisung Smart <jisung.smart@gmail.com>
2022-11-14 15:23:33 +0900

    Logo animation is adjust but svg animation isnot working correctly

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,50 +1,75 @@
-import LogoTitle from '../../assets/images/logo-s.png';
-import { Link } from 'react-router-dom';
-import './index.scss';
-import { useState, useEffect } from 'react';
-import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
-    const [ letterClass, setLetterClass] = useState('text-animate');
-    const nameArray = ['l','o','b','o','d','a','n'];
-    const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.'];
-    
-    useEffect(() => {
-        return setTimeout(() => {
-          setLetterClass('text-animate-hover')
-        }, 4000)
-      }, []);
-   
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    return (
-        <>
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <br/>
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}
-                 />
-                <br/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                 idx={22}   
-                />
-                </h1>
-                <h2> Fullstack Developer / Online business / Blogger </h2>
-                <Link to="/contack" className="flat-button">CONTACT ME</Link>
+  const nameArray = ['J', 'I', 'S', 'U', 'N', 'G', 'S']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
-            </div>
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
-        </>
+        <Logo />
+      </div>
 
-    )
+      <Loader type="pacman" />
+    </>
+  )
 }
 
-export default Home;
+export default Home
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,20 +5,18 @@ import './index.scss';
 const Layout = () => {
     return (
         <div className="App">
-            <Sidebar />
-            <div className='page'>
-                <span className="tags top-tags">&lt;body&gt;</span>
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
 
-                <Outlet />
-                <span className="tags bottom-tags">
-                    &lt;/body&gt;
-                    <br />
-                    <span className="bottom-tag-html">&lt;/html&gt;</span>
-                </span>
-                
-                
-            </div>
-        </div>
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
     )
 }
 
