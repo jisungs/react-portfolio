929cbf34277891ee162854946c90959848cb4239
Jisung Smart <jisung.smart@gmail.com>
2022-11-21 14:52:12 +0900

    Add contact page and make letter animation and connect to main app.js

---
+++ src/components/Contact/index.js
@@ -0,0 +1,39 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+
+
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        setTimeout(() => {
+         setLetterClass('text-animate-hover')
+       }, 4000)
+     }, [])
+
+    return (
+        <>
+            <div className=''>
+                 <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters 
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                            idx={15}
+                        />
+
+                    </h1>
+
+                 </div>
+
+            </div>
+            <Loader type='pacman'/>
+
+        </>
+    )
+}
+
+export default Contact
